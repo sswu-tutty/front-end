26a270741e76b137e540ac973193c77260879baa
jina ham <hamjina61@gmail.com>
2024-11-05 14:42:17 +0900

    style:요약본페이지 ui 구현완료

--- src/components/Editing.js
+++ src/components/Editing.js
@@ -1,9 +1,38 @@
-const Editing = () => {
+import { useState } from "react";
+import "./styles/Editing.css";
+import Back from "../assets/back1.png";
+
+const Editing = ({ detail, setEditing }) => {
+    const [title, setTitle] = useState(detail.title || "");
+    const [content, setContent] = useState(detail.content || "");
+
     return (
-        <div>
-            편집중
+        <div className="Editing">
+            <div className="btn_section">
+                <div className="back_button" onClick={() => setEditing(false)}>
+                    <img src={Back} />
+                    <span>뒤로가기</span>
+                </div>
+                <div className="done_btn" onClick={() => setEditing(false)}>
+                    완료
+                </div>
+            </div>
+            <div className="input_section">
+                <input
+                    className="input_title"
+                    placeholder="제목"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <textarea
+                    className="input_content"
+                    placeholder="내용"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                />
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Editing;
